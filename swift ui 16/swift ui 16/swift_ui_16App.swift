c2fa323ed87d7d786cef7f22d850d9c040480d54
Ilya Kazlou <ilya02k@gmail.com>
2024-05-21 16:09:58 +0300

    swift ui 16

---
+++ swift ui 16/swift ui 16/swift_ui_16App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_16App.swift
+//  swift ui 16
+//
+//  Created by Ann Dosova on 21.05.24.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
